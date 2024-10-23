cb054d2a1e848d78a8fca4fe05bdb021311e6d3b
Ant M <antm0sk@ya.ru>
2024-10-23 11:12:16 +0300

    first commit

---
+++ HW1/02_ladder_text.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Let\'s\n\tgo\n\t\tto walk\n");
+    return 0;
+}
---
+++ HW1/03_ascii_art.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Рисунок из звездочек
+    printf("     *\n");
+    printf("    ***\n");
+    printf("   *****\n");
+    printf("  *******\n");
+    
+    // Рисунок из букв
+    printf("   HHOHH\n");
+    printf("   ZZZZZ\n");
+    
+    return 0;
+}
